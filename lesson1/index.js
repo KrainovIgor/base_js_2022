c6f0bf5e0b465c65653f564c07a4855415a7c70b
KrainovIgor <kraynovigor86@gmail.com>
2022-02-19 11:20:48 +0300

    added first script

---
+++ lesson1/index.js
@@ -0,0 +1,22 @@
+console.log("HELLO JAVASCRIPT");
+
+const planetName="Earth";
+console.log(planetName); 
+
+const currentUsertime= new Date();
+console.log(currentUsertime.toLocaleTimeString());
+
+let articleQuantity="2";
+console.log(articleQuantity);
+
+let paymentType='cash';
+console.log(paymentType);
+
+let fullName="Иванов И.И.";
+console.log(fullName);
+
+let nameSurname="Петров П.П.";
+console.log(nameSurname);
+
+let firstSecondthirdName="Сидоров С.С.";
+console.log(firstSecondthirdName)
